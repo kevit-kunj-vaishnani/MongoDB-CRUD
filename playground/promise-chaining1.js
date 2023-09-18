b488701aa748c674078dab37b849b5426a8c6516
kevit-kunj-vaishnani <kevit.kunj.vaishnani@gmail.com>
2023-09-18 18:26:42 +0530

    made use of async-await in promise-chaining1.js and promise-chaining2.js

--- playground/promise-chaining1.js
+++ playground/promise-chaining1.js
@@ -1,13 +1,31 @@
+// require('../src/db/mongoose.js')
+// const User = require('../src/models/user.js')
+
+// User.findByIdAndUpdate('65040645f97b34625010681c' , { age : 90} )
+//     .then((user) => {
+//         console.log(user)
+//         return User.countDocuments({ age : 90 })
+//     })
+//     .then((result) => {
+//         console.log(result)
+//     })
+//     .catch((e) => {
+//         console.log(e);
+//     })
+
+
 require('../src/db/mongoose.js')
 const User = require('../src/models/user.js')
 
-User.findByIdAndUpdate('65040645f97b34625010681c' , { age : 90} )
-    .then((user) => {
-        console.log(user)
-        return User.countDocuments({ age : 90 })
-    })
-    .then((result) => {
-        console.log(result)
+const Update_Age_And_Count = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id , {age:age})   // first age is column/field name in database user model and second age is value user has given in function
+    const count = await User.countDocuments({age:age})
+    return count;
+}
+
+Update_Age_And_Count('65044222b9242e0ce0240d38', 24)
+    .then((count) => {
+        console.log(count);
     })
     .catch((e) => {
         console.log(e);
--- playground/promise-chaining2.js
+++ playground/promise-chaining2.js
@@ -1,13 +1,31 @@
+// require('../src/db/mongoose.js')
+// const Task = require('../src/models/task.js')
+
+// Task.findByIdAndDelete('65080701237a3e7554cd4e6d')
+//     .then((task) => {
+//         console.log(task);
+//         return Task.countDocuments({ completed : false })
+//     })
+//     .then((result) => {
+//         console.log(result)
+//     })
+//     .catch((e) => {
+//         console.log(e);
+//     })
+
+
 require('../src/db/mongoose.js')
 const Task = require('../src/models/task.js')
 
-Task.findByIdAndDelete('65080701237a3e7554cd4e6d')
-    .then((task) => {
-        console.log(task);
-        return Task.countDocuments({ completed : false })
-    })
-    .then((result) => {
-        console.log(result)
+const delete_Task_And_Count = async (id) => {
+    const task = await Task.findByIdAndDelete(id) // first id is column/field name in database in task model and second id is value user has given in function
+    const count = await Task.countDocuments( { completed : false })
+    return count;
+}
+
+delete_Task_And_Count('650846889f2382afbef9dc82')
+    .then((count) => {
+        console.log(count);
     })
     .catch((e) => {
         console.log(e);
